3d574269636598f56dabd42ea54477aacc6574a2
Jeffry Smith <smithjs9225@gmail.com>
2026-09-03 13:27:37 -0300

    Change default sort direction to DESC for orders

--- src/main/java/com/jeffry/ecommerce/controller/OrderController.java
+++ src/main/java/com/jeffry/ecommerce/controller/OrderController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,7 +29,7 @@ public class OrderController {
     @GetMapping
     public ResponseEntity<Page<OrderResponse>> findMyOrders(
             Authentication authentication,
-            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(orderService.findByUser(authentication.getName(), pageable));
     }
 
