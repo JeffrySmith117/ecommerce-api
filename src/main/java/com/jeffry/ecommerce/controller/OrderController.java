57b2cb87a5dd951d8162167012a982fd3ab8096a
Jeffry Smith <smithjs9225@gmail.com>
2026-09-03 13:21:22 -0300

    Change findMyOrders to return paginated results

--- src/main/java/com/jeffry/ecommerce/controller/OrderController.java
+++ src/main/java/com/jeffry/ecommerce/controller/OrderController.java
@@ -5,12 +5,13 @@ import com.jeffry.ecommerce.dto.OrderStatusUpdateRequest;
 import com.jeffry.ecommerce.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -25,8 +26,10 @@ public class OrderController {
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderResponse>> findMyOrders(Authentication authentication) {
-        return ResponseEntity.ok(orderService.findByUser(authentication.getName()));
+    public ResponseEntity<Page<OrderResponse>> findMyOrders(
+            Authentication authentication,
+            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.findByUser(authentication.getName(), pageable));
     }
 
     @GetMapping("/{id}")
