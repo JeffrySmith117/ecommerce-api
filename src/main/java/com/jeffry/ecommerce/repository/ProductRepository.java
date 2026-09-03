11445b4d592aa40a087fd2d772cffaa64019ca34
Jeffry Smith <smithjs9225@gmail.com>
2026-09-03 13:12:19 -0300

    Change product repository methods to return pages

--- src/main/java/com/jeffry/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/jeffry/ecommerce/repository/ProductRepository.java
@@ -1,11 +1,11 @@
 package com.jeffry.ecommerce.repository;
 
 import com.jeffry.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryId(Long categoryId);
-    List<Product> findByNameContainingIgnoreCase(String name);
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
